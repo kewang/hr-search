ec82ff42597e2be05f9891b258348848e874a743
kewang <cpckewang@gmail.com>
2015-10-03 18:35:48 +0800

    Add upload(submit) button

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var multer = require('multer')
+var upload = multer({
+  dest: 'uploads/'
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.post("/upload", function(req, res, next) {
+  console.log(req.file);
+  res.end();
 });
 
 module.exports = router;
