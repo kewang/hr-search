c22f4bff883cb117b025ea2a2af2fc762de7d35a
kewang <cpckewang@gmail.com>
2015-10-04 00:04:08 +0800

    Add Sequelize migration (uncomplete)

---
+++ migrations/20151003235652-create-employee-migration.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    queryInterface.createTable("Employee", {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      }
+    });
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+  }
+};
