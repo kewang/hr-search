5590061f0c639d6b5bda461ccede0a01e15c8b90
kewang <cpckewang@gmail.com>
2015-10-03 19:41:36 +0800

    Add name for multer

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var multer = require('multer')
+var multer = require('multer');
 var upload = multer({
   dest: 'uploads/'
 });
@@ -10,7 +10,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post("/upload", function(req, res, next) {
+router.post("/upload", upload.single("email"), function(req, res, next) {
   console.log(req.file);
   res.end();
 });
